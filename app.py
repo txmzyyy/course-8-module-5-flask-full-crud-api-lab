04ea99085683839885927d251e7993240c303540
txmzyyy <tamaramk08@gmail.com>
2026-06-26 14:44:59 +0300

    fixed code

--- app.py
+++ app.py
@@ -17,21 +17,31 @@ events = [
     Event(2, "Python Workshop")
 ]
 
-#GET method
+#GET methods
+@app.route('/', methods=["GET"])
+def home():
+    return jsonify({"message":"Welcome!"}), 200
+
 @app.route('/events', methods=["GET"])
-def get_event():
+def get_events():
     return (jsonify([event.to_dict()for event in events]), 200)
 
-@app.route('/events/<int:id>', methods=["GET"])
-def get_event(id)
-    event = next((e for e in events if e.id == id), None)
-    return jsonify(event.to_dict()) if event else ("Event not found", 404)
+@app.route('/events/<int:event_id>', methods=["GET"])
+def get_event(id):
+    event = next((e for e in events if e.id == event_id), None)
+    if not event:
+        return jsonify({"error":"Event not found"}), 404
+    return jsonify(event.to_dict()), 200
 
 
 # Create a new event from JSON input
 @app.route("/events", methods=["POST"])
 def create_event():
     data = request.get_json()
+
+    if not data or "title" not in data:
+        return jsonify({"error":"Title required"}), 400
+    
     new_id = max((e.id for e in events), default=0) + 1
     new_event = Event(id=new_id, title=data["title"])
     events.append(new_event)
@@ -44,19 +54,20 @@ def update_event(event_id):
     data= request.get_json()
     event = next((e for e in events if e.id == event_id), None)
     if not event:
-        return("Event not found", 404)
+        return jsonify({"error":"Event not found"}), 404
+    
     if "title" in data:
         event.title = data["title"]
-    return jsonify(event.to_dict())
+    return jsonify(event.to_dict()), 200
     
 
 # Remove an event from the list
 @app.route("/events/<int:event_id>", methods=["DELETE"])
 def delete_event(event_id):
     global events
-    event = next((e for e in events if e.id == id), None)
+    event = next((e for e in events if e.id == event_id), None)
     if not event:
-        return ("Event not found", 404)
+        return jsonify({"error":"Event not found"}), 404
     events = [e for e in events != event_id ]
     return "", 204
 
